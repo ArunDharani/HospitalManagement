31eed9ded290eb598e47700803c902fde2ff9e60
ArunDharani <myimportantworks2@gmail.com>
2026-02-04 08:12:16 +0530

    trying to pull data

--- src/main/java/com/HospitalManagement/Services/Service_Patient.java
+++ src/main/java/com/HospitalManagement/Services/Service_Patient.java
@@ -10,9 +10,6 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.request.async.AsyncRequestNotUsableException;
-
-import javax.print.DocFlavor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
