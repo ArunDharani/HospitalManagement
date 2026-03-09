4c8bf9018e964b8c2868e1b1f06ad4c53d7705e7
ArunDharani <myimportantworks2@gmail.com>
2026-03-09 09:43:26 +0530

    Added job for auto delete of request based on 'email' within 10 minutes

--- src/main/java/com/HospitalManagement/RepositoryInterfaces/RequestRepository.java
+++ src/main/java/com/HospitalManagement/RepositoryInterfaces/RequestRepository.java
@@ -2,9 +2,16 @@
 
 // Importing the necessary packages
 package com.HospitalManagement.RepositoryInterfaces;
-import com.HospitalManagement.Entities.TestData;
+import com.HospitalManagement.Entities.request;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 // Creation of interfaces
-public interface RequestRepository extends JpaRepository<TestData , Long> {
+public interface RequestRepository extends JpaRepository<request, Long> {
+    @Modifying
+    @Query("delete from request where email = :value ")
+    void delete(@Param("value") String value);
 }
